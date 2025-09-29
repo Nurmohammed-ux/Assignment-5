8fb4dd03ed9dfe77b6f8ca298d25c5927578f747
Nurmohammed-ux <hridoynmh@gmail.com>
2025-09-29 13:35:06 +0600

    add code about copy button

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ hearts.forEach(function (event) {
 // Append New History function
 
 function addToHistory(serviceName, serviceNumber) {
-  const history = document.getElementById("call-history");
+  const historyList = document.getElementById("history-list");
 
   const div = document.createElement("div");
   div.className =
@@ -35,8 +35,16 @@ function addToHistory(serviceName, serviceNumber) {
   div.appendChild(left);
   div.appendChild(serviceTime);
 
-  history.appendChild(div);
+  historyList.appendChild(div);
 }
+
+// Clear History Functionality
+
+document.getElementById("clear-history").addEventListener("click", function () {
+  const historyList = document.getElementById("history-list");
+  historyList.innerText = "";
+  historyList.appendChild(div);
+});
 
 // Call Buttons functionality
 
@@ -61,4 +69,64 @@ buttons.forEach(function (event) {
 
     addToHistory(serviceName, serviceNumber);
   });
+});
+
+// Copy Buttons function
+
+function copyToClipboard(text) {
+  if (navigator.clipboard && window.isSecureContext) {
+    return navigator.clipboard.writeText(text);
+  } else {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    textArea.style.position = "fixed";
+    textArea.style.left = "-9999px";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    return Promise.resolve();
+  }
+}
+
+// Copy Button Handler functionality
+
+document.addEventListener("click", function (e) {
+  const btn = e.target.closest(".copy-btn");
+  if (!btn) return;
+
+  const card = btn.closest(".style-card");
+  const targetSelector = btn.getAttribute("data-copy-target");
+  const targetEl = card.querySelector(targetSelector);
+
+  console.log(card,targetSelector,targetEl)
+
+  if (targetEl) {
+    const text = targetEl.innerText.trim();
+
+    copyToClipboard(text).then(function () {
+      const span = btn.querySelector("span");
+
+      if (!span) {
+        const span = document.createElement("span");
+        span.innerText = " Copy";
+        btn.appendChild(span);
+      }
+
+      const originalText = span.innerText;
+      span.innerText = " Copied!";
+
+      btn.classList.add("bg-green-600", "text-white");
+      btn.classList.remove("bg-white", "text-gray-500");
+
+      setTimeout(function () {
+        span.textContent = originalText;
+        btn.classList.remove("bg-green-600", "text-white");
+        btn.classList.add("bg-white", "text-gray-500");
+      }, 1500);
+    });
+  }
 });
